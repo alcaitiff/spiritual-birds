04f0cdd22a0d477698845acab1f3bacc15e02dac
Alan Rabello <alan.rabello@serpro.gov.br>
2020-12-11 17:01:54 -0300

    Refactory to get more control over enemies

--- src/static/js/phaser/Enemies.js
+++ src/static/js/phaser/Enemies.js
@@ -1,40 +1,58 @@
 import GrayBird from './GrayBird';
 const Enemies = {
-  enemies: [],
-  enemyGroup: null,
-  numEnemies: 3,
-  player: null,
   create(scene, player) {
-    this.player = player;
-    this.enemyGroup = scene.physics.add.group();
-    scene.physics.add.collider(
-      player.arcadeSprite,
-      this.enemyGroup,
-      (player, enemy) => {
-        player.control.hit(scene, enemy.control.dmg);
-        enemy.control.bounce();
+    const newObj = {
+      Types: {
+        GrayBird: {
+          min: 3,
+          alive: [],
+          killed: 0,
+          fleed: 0,
+          create: GrayBird.create.bind(GrayBird)
+        }
+      },
+      enemyGroup: null,
+      player: null,
+      construct(scene, player) {
+        this.player = player;
+        this.enemyGroup = scene.physics.add.group();
+        scene.physics.add.collider(
+          player.arcadeSprite,
+          this.enemyGroup,
+          (player, enemy) => {
+            player.control.hit(scene, enemy.control.dmg);
+            enemy.control.bounce();
+          }
+        );
+        scene.physics.add.collider(
+          player.bulletGroup,
+          this.enemyGroup,
+          (bullet, enemy) => {
+            bullet.control.hit();
+            const points = enemy.control.hit(bullet.control.dmg, player);
+            player.addScore(points);
+          }
+        );
+        return this;
+      },
+      generate(type) {
+        while (type.alive.length < type.min) {
+          type.alive.push(type.create(scene, this.enemyGroup));
+        }
+      },
+      updateType(type) {
+        type.alive.forEach((element, index) => {
+          if (element === null || !element.update()) {
+            type.alive.splice(index, 1);
+          };
+        });
+      },
+      update(scene) {
+        this.generate(this.Types.GrayBird);
+        this.updateType(this.Types.GrayBird);
       }
-    );
-    scene.physics.add.collider(
-      player.bulletGroup,
-      this.enemyGroup,
-      (bullet, enemy) => {
-        bullet.control.hit();
-        const points = enemy.control.hit(bullet.control.dmg, player);
-        player.addScore(points);
-      }
-    );
-    return this;
-  },
-  update(scene) {
-    while (this.enemies.length < this.numEnemies) {
-      this.enemies.push(GrayBird.create(scene, this.enemyGroup));
-    }
-    this.enemies.forEach((element, index) => {
-      if (element === null || !element.update()) {
-        this.enemies.splice(index, 1);
-      };
-    });
+    };
+    return newObj.construct(scene, player);
   }
 };
 export default Enemies;
