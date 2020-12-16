7b7c51bacf47b96ae6bf17515db2ba2c321a712f
Alan Rabello <alan.rabello@serpro.gov.br>
2020-12-16 17:09:13 -0300

    Preloading

--- src/static/js/phaser/scenes/Preload.js
+++ src/static/js/phaser/scenes/Preload.js
@@ -1,6 +1,66 @@
 const Preload = {
   key: 'Preload',
   preload() {
+    var progressBar = this.add.graphics();
+    var progressBox = this.add.graphics();
+    var width = this.cameras.main.width;
+    var height = this.cameras.main.height;
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(width / 2 - 160, height / 2 - 25, 320, 50);
+
+    var loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff'
+      }
+    });
+    loadingText.setOrigin(0.5, 0.5);
+
+    var percentText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 5,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff'
+      }
+    });
+    percentText.setOrigin(0.5, 0.5);
+
+    var assetText = this.make.text({
+      x: width / 2,
+      y: height / 2 + 50,
+      text: '',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff'
+      }
+    });
+
+    assetText.setOrigin(0.5, 0.5);
+
+    this.load.on('progress', function(value) {
+      percentText.setText(parseInt(value * 100) + '%');
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(width / 2 - 155, height / 2 - 20, 300 * value, 35);
+    });
+
+    this.load.on('fileprogress', function(file) {
+      assetText.setText('Loading asset: ' + file.key);
+    });
+
+    this.load.on('complete', function() {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+      assetText.destroy();
+    });
+
     // load all assets tile sprites
     this.load.image('bg_1', 'assets/parallax/lvl1/sky.png');
     this.load.image('bg_2', 'assets/parallax/lvl1/decor.png');
